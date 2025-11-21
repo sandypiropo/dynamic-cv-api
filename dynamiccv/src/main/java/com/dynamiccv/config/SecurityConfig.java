cbfbd24cc837fb53d68bffd3fbb5cb5899a2115d
Sandy Piropo <sandypiropo@gmail.com>
2025-11-21 19:26:27 -0300

    add config

--- dynamiccv/src/main/java/com/dynamiccv/config/SecurityConfig.java
+++ dynamiccv/src/main/java/com/dynamiccv/config/SecurityConfig.java
@@ -7,4 +7,22 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/index.html"
+                        ).permitAll()
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
 }
+
