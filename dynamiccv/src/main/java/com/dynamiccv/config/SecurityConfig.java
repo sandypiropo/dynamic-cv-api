8e5a9f648af7e89508f60bba10969c3d30436d51
Sandy Piropo <sandypiropo@gmail.com>
2025-11-21 19:18:29 -0300

    Init project

---
+++ dynamiccv/src/main/java/com/dynamiccv/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.dynamiccv.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+}
---
+++ dynamiccv/src/test/java/com/dynamiccv/DynamiccvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamiccv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamiccvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
