4a2875dc2cf95c5d8193c656a449a6fc38317d32
SPIROPO_ford <spiropo@ford.com>
2025-11-22 19:05:05 -0300

    Swagger user doc

--- dynamiccv/src/main/java/com/dynamiccv/controller/UserController.java
+++ dynamiccv/src/main/java/com/dynamiccv/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.dynamiccv.controller;
 
 import com.dynamiccv.dto.UserDTO;
-import com.dynamiccv.model.User;
 import com.dynamiccv.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,30 +20,39 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get user by ID", description = "Retrieve a single user by their ID")
     @GetMapping("/{id}")
-    public UserDTO getUserById(@PathVariable Long id) {
+    public UserDTO getUserById(
+            @Parameter(description = "ID of the user to retrieve") @PathVariable Long id) {
         return userService.getUserById(id);
     }
 
+    @Operation(summary = "Create a new user", description = "Create a new user with all required details")
     @PostMapping
-    public UserDTO createUser(@Valid @RequestBody UserDTO userDTO) {
+    public UserDTO createUser(
+            @Parameter(description = "User data to create") @Valid @RequestBody UserDTO userDTO) {
         return userService.createUser(userDTO);
     }
 
+    @Operation(summary = "Get all users", description = "Retrieve a list of all users")
     @GetMapping
     public List<UserDTO> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @Operation(summary = "Update a user", description = "Update an existing user by ID")
     @PutMapping("/{id}")
-    public UserDTO updateUser(@PathVariable Long id, @Valid @RequestBody UserDTO userDTO) {
+    public UserDTO updateUser(
+            @Parameter(description = "ID of the user to update") @PathVariable Long id,
+            @Parameter(description = "Updated user data") @Valid @RequestBody UserDTO userDTO) {
         return userService.updateUser(id, userDTO);
     }
 
+    @Operation(summary = "Delete a user", description = "Delete a user by ID")
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT) // retorna 204 se sucesso
-    public void deleteUser(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(
+            @Parameter(description = "ID of the user to delete") @PathVariable Long id) {
         userService.deleteUser(id);
     }
-
 }
